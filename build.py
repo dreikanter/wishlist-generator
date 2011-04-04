9136e7a8b6d2a2d511c8491fc12512579444cd4e
Alex Musayev <alex.musayev@gmail.com>
2011-04-05 01:51:54 +0400

    Images retrieving and processing done.

--- build.py
+++ build.py
@@ -7,7 +7,6 @@ import shutil
 import csv
 #from django.template import Context, Template
 #from django.conf import settings
-import urlparse
 
 # read source file
 # read cache if exists
@@ -24,122 +23,100 @@ import urlparse
 # generate html
 
 
+class WishlistConf:
+    def __init__(self, conf_file, section_name):
+        try:
+            conf = ConfigParser.ConfigParser()
+            conf.read(conf_file)
+
+            self.sourceFile = conf.get(section_name, 'source_file')
+            self.dataFile = conf.get(section_name, 'data_file')
+            self.tmpPath = conf.get(section_name, 'tmp_path')
+            self.imagePath = conf.get(section_name, 'image_path')
+            self.identifyCmd = conf.get(section_name, 'identify_cmd')
+            self.mogrifyCmd = conf.get(section_name, 'mogrify_cmd')
+            self.csvDelimiter = conf.get(section_name, 'csv_delimiter')
+            self.maxImageWidth = int(conf.get(section_name, 'max_image_width'))
+            self.maxImageHeight = int(conf.get(section_name, 'max_image_height'))
+
+        except:
+            print "Error reading configuration file '%s' (section: '%s')" % (conf_file, section_name)
+            raise
+
+class Logger:
+    def __init__(self, file_name, show_messages = False):
+        self.file_name = file_name
+        self._show = show_messages
+
+    def write(self, message):
+        if self._show: print(message)
+
 class Wishlist:
-
-    class Conf:
-        def __init__(self, conf_file, section_name):
-            try:
-                self._conf = ConfigParser.ConfigParser()
-                self._conf.read(conf_file)
-                self.sourceFile = self._conf.get(section_name, 'source_file')
-                self.dataFile = self._conf.get(section_name, 'data_file')
-                self.tmpPath = self._conf.get(section_name, 'tmp_path')
-                self.imagePath = self._conf.get(section_name, 'image_path')
-                self.identifyCmd = self._conf.get(section_name, 'identify_cmd')
-                self.mogrifyCmd = self._conf.get(section_name, 'mogrify_cmd')
-                self.csvDelimiter = self._conf.get(section_name, 'csv_delimiter')
-            except:
-                print "Error reading configuration file '%s' (section: '%s')" % (conf_file, section_name)
-                raise
-
-    def __init__(self, conf_file, conf_section = 'wishlist-builder'):
-        self._conf = WishlistParocessor.Conf(conf_file, conf_section)
+    def __init__(self, conf, log):
+        self._conf = conf
+        self._log = log
 
     def generate(self):
-        print self._conf
+        self._data = self.merge_data(self.read_source(), self.load_data())
+        self.download_new_images()
+        self.process_images()
+        self.save_data()
         return
 
-    def read_source_file(self, source_file):
+    def read_source(self):
         '''Returns a collection of { url, image_url, description } tuples'''
-        if not os.path.exists(source_file):
-            print('Source file does not exists: ' + source_file)
+        if not os.path.exists(self._conf.sourceFile):
+            print('Source file does not exists: ' + self._conf.sourceFile)
             return []
 
         try:
             result = []
-            for item in open(source_file, 'r').read().strip().split('\n\n'):
+            for item in open(self._conf.sourceFile, 'r').read().strip().split('\n\n'):
                 parts = item.split('\n')
                 if len(parts) != 3: continue
                 description, url, image_url = parts
-                result.append({ 'url':url.strip(),
-                                'image_url':image_url.strip(),
-                                'desc':description.strip() })
+                result.append({ 'url':url.strip(), 'image_url':image_url.strip(), 'desc':description.strip() })
             return result
 
         except Exception as ex:
             print('Error reading source file: ' + str(ex))
             return []
 
-    def get_image_size(self, images_list):
-        '''Returns a collection of [width, height] for a set of image files'''
-        try:
-            result = []
-            for line in os.popen(self._conf.identifyCmd % ' '.join(images_list)).readlines():
-                parts = line.split(',')
-                if len(parts) != 3: continue
-                file_name, width, height = parts
-                result.append([file_name.strip(), int(width.strip()), int(height.strip())])
-            return result
-
-        except Exception as ex:
-            print('Error getting image resolution: ' + str(ex))
-            return []
-
-    def download(self, url, fileName = None):
-        '''Downloads and save a file from URL'''
-        def getFileName(url, openUrl):
-            if 'Content-Disposition' in openUrl.info():
-                cd = dict(map(
-                    lambda x: x.strip().split('=') if '=' in x else (x.strip(),''),
-                    openUrl.info().split(';')))
-                if 'filename' in cd:
-                    filename = cd['filename'].strip("\"'")
-                    if filename: return filename
-            return os.path.basename(urlparse.urlsplit(openUrl.url)[2])
-
-        r = urllib2.urlopen(urllib2.Request(url))
-        try:
-            fileName = fileName or getFileName(url, r)
-            with open(fileName, 'wb') as f:
-                shutil.copyfileobj(r, f)
-        finally:
-            r.close()
-
-    def save_data(self, csv_file, data):
+    def save_data(self):
         '''Save wishlist records to CSV file'''
         def to_csv_row(record):
             try:
-                return [record['url'], record['image_url'], record['image_id'],
+                return [record['url'], record['image_url'], record['image_file'],
                         record['width'], record['height'], record['desc']]
             except:
                 return None
 
         try:
-            writer = csv.writer(open(csv_file, 'wb'), delimiter = self._conf.csvDelimiter, quoting = csv.QUOTE_MINIMAL)
-            for row in [to_csv_row(record) for record in data]:
+            writer = csv.writer(open(self._conf.dataFile, 'wb'), delimiter = self._conf.csvDelimiter, quoting = csv.QUOTE_MINIMAL)
+            for row in [to_csv_row(record) for record in self._data]:
                 if row is not None: writer.writerow(row)
 
         except Exception as ex:
-            print('Error saving data to %s: %s' % (csv_file, str(ex)))
+            print('Error saving data to %s: %s' % (self._conf.dataFile, str(ex)))
 
-    def load_data(self, csv_file):
+    def load_data(self):
         '''Load wishlist records from CSV file'''
         def to_dict(row):
             try:
-                return { 'url':row[0], 'image_url':row[1], 'image_id':row[2],
-                         'width':row[3], 'height':row[4], 'desc':row[5] }
+                return { 'url':row[0], 'image_url':row[1], 'image_file':row[2],
+                         'width':int(row[3]), 'height':int(row[4]), 'desc':row[5] }
             except:
                 return None
 
         try:
             result = []
-            reader = csv.reader(open(csv_file, 'rb'), delimiter = self._conf.csvDelimiter)
+            reader = csv.reader(open(self._conf.dataFile, 'rb'), delimiter = self._conf.csvDelimiter)
             for record in [to_dict(row) for row in reader]:
                 if record is not None: result.append(record)
             return result
 
         except Exception as ex:
-            print('Error loading data from %s: %s' % (csv_file, str(ex)))
+            print('Error loading data from %s: %s' % (self._conf.dataFile, str(ex)))
 
     def merge_data(self, new_data, cached_data):
         '''Merge new data from source file and cached data'''
@@ -155,39 +132,95 @@ class Wishlist:
             existing = get_existing(cached_data)
             if existing is not None:
                 if existing['image_url'] != item['image_url']:
-                    existing.update({'image_id':0, 'width':0, 'height':0})
+                    existing.update({'image_file':'', 'width':0, 'height':0})
                 existing.update(item)
                 result.append(existing)
             else:
-                item.update({'image_id':0, 'width':0, 'height':0})
+                item.update({'image_file':'', 'width':0, 'height':0})
                 result.append(item)
         return result
 
-    def download_images(self, data):
-        '''Download images for wishlist records and returns { image_url => [image_id, file_name] } dict'''
+    def download_new_images(self):
+        '''Download images for wishlist records and returns a collection of new image IDs'''
+
+        def download(url, file_name):
+            '''Downloads and save a file from URL'''
+            r = urllib2.urlopen(urllib2.Request(url))
+            ext = r.headers.subtype.lower()
+            if ext not in ['jpeg', 'png', 'gif']: return None
+            save_as = '%s.%s' % (file_name, ext)
+
+            try:
+                with open(save_as, 'wb') as f: shutil.copyfileobj(r, f)
+            finally:
+                r.close()
+
+            return save_as
+
         counter = 1
         result = []
-        for item in data:
-            if int(item['image_id']): continue
+        for item in self._data:
+            if len(item['image_file']): continue
             while True:
                 new_file = os.path.join(self._conf.imagePath, str(counter))
                 if len(glob.glob(new_file + '.*')) < 1 and not os.path.exists(new_file): break;
                 counter += 1
-            ext = os.path.splitext(urlparse.urlparse(item['image_url']).path)[1].lower()
-            save_as = os.path.join(self._conf.imagePath, str(counter) + ext)
-            print '%s -> %s' % (item['image_url'], save_as)
-            self.download(item['image_url'], save_as)
-            item['image_id'] = counter
-            result.append(save_as)
+            print 'Downloading %s (%d)' % (item['image_url'], counter)
+            new_image = download(item['image_url'], os.path.join(self._conf.imagePath, str(counter)))
+            if new_image:
+                result.append(new_image)
+                item['image_file'] = os.path.basename(new_image)
+            else:
+                print('Error downloading file')
+
         return result
 
-    def scale_images(self, max_width, max_height):
-        os.popen(self._conf.mogrifyCmd % (max_width, max_height, os.path.join(self._conf.imagePath, '*')))
-        return
+    def process_images(self):
+        '''Scales new images and retrieves image sizes'''
+
+        def set_image_size(image_file, width, height):
+            for item in self._data:
+                if item['image_file'] == image_file:
+                    item['width'] = width
+                    item['height'] = height
+                    break
+
+        try:
+            image_files = [self._conf.imagePath + item['image_file'] for item in self._data
+                if (item['width'] == 0 or item['height'] == 0) and item['image_file']]
+            self.execute(self._conf.mogrifyCmd % (self._conf.maxImageWidth,
+                self._conf.maxImageHeight, os.path.join(self._conf.imagePath, '*')))
+
+            lines = self.execute(self._conf.identifyCmd % ' '.join(image_files), True)
+            for line in lines:
+                parts = line.strip().split(',')
+                if len(parts) != 3: continue
+                set_image_size(parts[0], int(parts[1]), int(parts[2]))
+
+        except Exception as ex:
+            print('Error getting image resolution: ' + str(ex))
+            return []
+
+    def execute(self, command, get_output = False):
+        print 'Executing command: ' + command
+        h = os.popen(command)
+        if get_output: return h.readlines()
+
+#    def get_image_size(self, image_file):
+#        try:
+#            output = os.popen(self._conf.identifyCmd % image_file).readlines()
+#            parts = output[0].strip().split(',')
+#            if len(parts) != 3: raise Exception('Identify output: "%s"' % output)
+#            width, height = parts[1], parts[2]
+#            return int(width), int(height)
+#
+#        except Exception as ex:
+#            print('Error getting image size for "%s": %s' % (image_file, str(ex)))
+#            return 0, 0
 
 
 
-Wishlist('build.conf').generate()
+Wishlist(WishlistConf('build.conf', 'wishlist-builder'), Logger('wishlist.log')).generate()
 
 
 
